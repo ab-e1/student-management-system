484a533d6c17ee50a9fcab4f9dbb3ab1ab62cfee
ab-e1 <abelghy17@gmail.com>
2026-08-12 22:04:08 +0300

    created one the array for storing the data, and worte the nextID function for appending od when data is created

--- data/students.js
+++ data/students.js
@@ -1,1 +1,17 @@
+const students = [
+  {
+    id: 1,
+    name: "Amina Hassen",
+    email: "amina@gmail.com",
+    course: "computer science",
+    age: 21,
+    gpa: 3.7,
+    status: "Active",
+  },
+];
 
+const nextId = () => {
+  return Math.max(...students.map((students) => students.id)) + 1;
+};
+
+module.exports = { students, nextId };
