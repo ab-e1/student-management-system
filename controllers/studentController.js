b3478d7e45e43b3b118b955c503288814b87b99d
ab-e1 <abelghy17@gmail.com>
2026-08-13 20:47:57 +0300

    did the filter search by name, and filterby course as a different functions but jsut recoginized all shoud be in one route so going to chage that

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -41,3 +41,81 @@ const updateStudent = (req, res, next) => {
     message: "Student succesfully updated",
   });
 };
+
+const getAllStudents = (req, res, next) => {
+  if (!students) {
+    const error = new Error("There are no students in the database");
+    error.status = 404;
+    return next(error);
+  } else {
+    res.status(200).json(students);
+  }
+};
+
+const getStudentByID = (req, res, next) => {
+  const student = students.find((s) => s.id === Number(req.params.id));
+  if (!student) {
+    const error = new Error("student not found");
+    error.status = 404;
+    return next(error);
+  } else {
+    res.status(200).json(student);
+  }
+};
+
+const getStudentByName = (req, res, next) => {
+  const student = students.filter((s) =>
+    s.name.toLowerCase().includes(req.query.name.toLowerCase()),
+  );
+  if (!student) {
+    const error = new Error("student not found");
+    error.status = 404;
+    return next(error);
+  } else {
+    res.status(200).json(student);
+  }
+};
+
+const getStudentByEmail = (req, res, next) => {
+  const student = students.filter((s) =>
+    s.email.toLowerCase().includes(req.query.email.toLowerCase()),
+  );
+  if (!student) {
+    const error = new Error("Student not found");
+    error.status = 404;
+    return next(error);
+  } else {
+    res.status(200).json(student);
+  }
+};
+
+const deleteStudent = (req, res, next) => {
+  const index = students.findIndex((s) => s.id === Number(req.params.id));
+  if (index === Number(-1)) {
+    const error = new Error("Student not found");
+    error.status = 404;
+    return next(error);
+  } else {
+    const deleted = students.splice(index, 1);
+    res.status(200).json({
+      deleted,
+      message: "Student deleted succesfully",
+    });
+  }
+};
+
+const filterByCourse = (req, res, next) => {
+  const student = students.filter((s) =>
+    s.course.toLowerCase().includes(req.query.course.toLowerCase()),
+  );
+  if (!student) {
+    const error = new Error("Students not found");
+    error.status = 404;
+    return next(error);
+  } else {
+    res.status(200).json({
+      student,
+      message: "filtered by course succesfully",
+    });
+  }
+};
