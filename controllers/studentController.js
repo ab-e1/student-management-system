896b40fb2c5073589f1dd6e4c9bf15ed6567bf12
ab-e1 <abelghy17@gmail.com>
2026-08-13 17:10:33 +0300

    creating , and updating a student logic implemented , also error handeling for the updateStudents

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,1 +1,43 @@
+const { students, nextId } = require("../data/students.js");
 
+const createStudent = (req, res, next) => {
+  const { name, email, course, age, gpa, status } = req.body;
+
+  const newStudent = {
+    id: nextId(),
+    name: name,
+    email: email,
+    age: age,
+    course: course,
+    gpa: gpa,
+    status: status.trim().toLowerCase(),
+  };
+  students.push(newStudent);
+  res
+    .status(201)
+    .json({ newStudent, message: "new student succesfully created" });
+};
+
+const updateStudent = (req, res, next) => {
+  const student = students.find((s) => s.id === Number(req.params.id));
+
+  if (!student) {
+    const error = new Error("student not found");
+    error.status = 404;
+    return next(error);
+  }
+
+  const { name, email, course, age, gpa, status } = req.body;
+  student.id = student.id;
+  student.name = name;
+  student.email = email;
+  student.age = age;
+  student.course = course;
+  student.gpa = gpa;
+  student.status = status;
+  //
+  res.status(200).json({
+    updatedStident: student,
+    message: "Student succesfully updated",
+  });
+};
